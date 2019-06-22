3965dc5b3b805bcc0f99daa7773bf146a520f67b
sun-23 <twinsamson@gmail.com>
2019-06-22 20:19:25 +0700

    Initial Commit

---
+++ SwiftUI_ListAndNavigation/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SwiftUI_ListAndNavigation
+//
+//  Created by sun on 22/6/2562 BE.
+//  Copyright © 2562 sun. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
